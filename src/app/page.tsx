0e23c28ce5d1c7465eabb057f24f335caef30db7
Fayssal Ben Hammou <fayssal.benhammou@gmail.com>
2024-10-20 13:32:38 +0200

    styles: design, theme and background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,157 @@
+"use client"; // Ajoute cette ligne en haut du fichier
+
+import ParticlesContainer from "@/components/animations/particles";
+import { motion } from "framer-motion";
+import { Github, Linkedin, FileUser } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="min-h-screen flex flex-col bg-gray-900 bg-opacity-90 text-gray-100 relative">
+      {/* AppBar avec fond transparent et les onglets */}
+      <div className="absolute top-0 left-0 right-0 flex justify-end p-4 bg-transparent z-10">
+        <nav>
+          <ul className="flex space-x-8 text-gray-300">
+            <motion.li
+              whileHover={{ scale: 1.1, color: "green" }}
+              transition={{ duration: 0.2 }}
+            >
+              <a href="#projects" className="hover:text-green-500">
+                Projects
+              </a>
+            </motion.li>
+            <motion.li
+              whileHover={{ scale: 1.1, color: "green" }}
+              transition={{ duration: 0.2 }}
+            >
+              <a href="#skills" className="hover:text-green-500">
+                Skills
+              </a>
+            </motion.li>
+            <motion.li
+              whileHover={{ scale: 1.1, color: "green" }}
+              transition={{ duration: 0.2 }}
+            >
+              <a href="#blog" className="hover:text-green-500">
+                Blog
+              </a>
+            </motion.li>
+            <motion.li
+              whileHover={{ scale: 1.1, color: "green" }}
+              transition={{ duration: 0.2 }}
+            >
+              <a href="#contact" className="hover:text-green-500">
+                Contact
+              </a>
+            </motion.li>
+          </ul>
+        </nav>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      {/* Section principale contenant le contenu et l'image */}
+      <div className="flex flex-grow">
+        {/* Section gauche avec le contenu */}
+        <div className="w-1/2 flex flex-col justify-center items-start p-10 space-y-8">
+          <motion.h1
+            className="text-4xl sm:text-6xl font-bold text-white"
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Fayssal Ben Hammou
+          </motion.h1>
+          <motion.div
+            className="max-w-lg"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.4, duration: 0.7 }}
           >
-            Read our docs
-          </a>
+            <p className="text-lg sm:text-xl text-gray-300 mb-4">
+              I am a passionate Full Stack Developer with a strong background in
+              web and mobile technologies. I specialize in{" "}
+              <span className="font-semibold text-white">Tailwind CSS</span> for
+              designing responsive, modern interfaces, and{" "}
+              <span className="font-semibold text-white">Flutter</span> for
+              building cross-platform mobile applications. I also have
+              experience with back-end development using Node.js, Express, and
+              other tools.
+            </p>
+            <p className="text-lg sm:text-xl text-gray-300">
+              My goal is to create efficient, scalable, and user-friendly
+              applications that solve real-world problems. Let's build something
+              great together!
+            </p>
+          </motion.div>
+
+          {/* Icônes des réseaux sociaux */}
+          <motion.div
+            className="flex items-center space-x-4"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+          >
+            <a
+              href="https://github.com/ton-utilisateur"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-100 hover:text-green-500 transition-colors duration-300"
+            >
+              <Github
+                size={40}
+                className="hover:scale-110 transition-transform duration-300"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/ton-utilisateur/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-100 hover:text-green-500 transition-colors duration-300"
+            >
+              <Linkedin
+                size={40}
+                className="hover:scale-110 transition-transform duration-300"
+              />
+            </a>
+            <a
+              href="https://drive.google.com/file/d/11DdiGojGpYaCfvF72nTt0LfOJOMGIMST/view"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-100 hover:text-green-500 transition-colors duration-300"
+            >
+              <FileUser
+                size={40}
+                className="hover:scale-110 transition-transform duration-300"
+              />
+            </a>
+          </motion.div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        {/* Section droite avec l'image */}
+        <div className="w-1/2 flex justify-center items-center">
+          <div className="relative w-[80%] h-[80%] bg-no-repeat bg-right bg-contain">
+            <Image
+              src="/images/mask-OFF.png"
+              layout="fill"
+              objectFit="contain"
+              alt="background home page"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Footer */}
+      <footer className="bg-transparent py-6 text-center w-full">
+        <motion.p
+          className="text-gray-300"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.5 }}
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+          © 2024 Fayssal Ben Hammou. All rights reserved.
+        </motion.p>
       </footer>
+
+      <ParticlesContainer />
     </div>
   );
 }
---
+++ src/components/animations/particles/index.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { useCallback } from "react";
+import type { Container, Engine } from "tsparticles-engine";
+import Particles from "react-tsparticles";
+//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+
+const ParticlesContainer = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    console.log(engine);
+
+    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    //await loadFull(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    []
+  );
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      className="z-50"
+      options={{
+        background: {
+          color: {
+            value: "",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "34D399",
+          },
+          links: {
+            color: "BEC9E2",
+            distance: 150,
+            enable: true,
+            opacity: 0.1,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 950,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.2,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+};
+
+export default ParticlesContainer;
